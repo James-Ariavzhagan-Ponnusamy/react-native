7d8f722cefbb8130ae391d626345dc5ac2e3ae42
James Arivazhagan <James.Arivazhagan@gmail.com>
2021-08-05 21:02:06 +0530

    Create prog11.js

---
+++ prog11.js
@@ -0,0 +1,36 @@
+
+
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native'
+
+class Separator extends Component
+{
+  render()
+  {
+    return(
+      <Text> Break !!!! </Text>
+    )
+  }
+}
+
+class main_prg extends Component{
+  render()
+  {
+    return(
+      <View>
+      <Separator/>
+      <Separator/>
+      <Separator/>
+      <Separator/>
+      <Separator/>
+
+      <Text>
+        Inside the data
+      </Text>
+      </View>
+    );
+  }
+}
+
+export default main_prg
