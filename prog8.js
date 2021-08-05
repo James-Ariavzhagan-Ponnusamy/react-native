2982fa42ad5946dd9a04af643fc2917cfdbc8952
James Arivazhagan <James.Arivazhagan@gmail.com>
2021-08-05 20:48:02 +0530

    Create prog8.js

---
+++ prog8.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import {Text} from 'react-native'
+
+class Cat extends Component
+{
+  render()
+  {
+    return(
+      <Text>Hello, I am your first Cat ! </Text>
+
+    );
+
+  }
+
+}
+
+const Cat1 = () => {
+  return(
+    <Text>Test with function </Text>
+
+  )
+
+}
+
+//export default Cat;
+export default Cat1;
